1feeadff37fd3eb9bd16e5b0c5ecd7d6797d1b1f
Rajat Jindal <rajatjindal83@gmail.com>
2025-04-30 10:56:27 +0530

    add example to show error when streaming data more than 4096 bytes
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

---
+++ examples/streamdata/streamdata.go
@@ -0,0 +1,55 @@
+// This example is taken from https://github.com/dev-wasm/dev-wasm-go/blob/main/http/main.go
+// demonstrates how to use the wasihttp package to make HTTP requests using the `http.Client` interface.
+//
+// To run: `tinygo build -target=wasip2-roundtrip.json -o streamdata.wasm ./examples/streamdata`
+// Test: `wasmtime run -Shttp -Sinherit-network -Sinherit-env streamdata.wasm`
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"io"
+	"net/http"
+
+	wasihttp "github.com/ydnar/wasi-http-go/wasihttp"
+)
+
+//go:embed bigdata_10kb.data
+var bigdata []byte
+
+func printResponse(r *http.Response) error {
+	fmt.Printf("Status: %d\n", r.StatusCode)
+	for k, v := range r.Header {
+		fmt.Printf("%s: %s\n", k, v[0])
+	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Body: \n%s\n", body)
+	return nil
+}
+
+func main() {
+	client := &http.Client{
+		Transport: &wasihttp.Transport{},
+	}
+	req, err := http.NewRequest("POST", "https://postman-echo/post", bytes.NewReader(bigdata))
+	if err != nil {
+		panic(err.Error())
+	}
+	if req == nil {
+		panic("Nil request!")
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer res.Body.Close()
+
+	err = printResponse(res)
+	if err != nil {
+		panic(err.Error())
+	}
+}
